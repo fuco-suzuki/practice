504e34e00b1e1e466ae4735c8f8cae682885f294
Fuco Suzuki <fuco-suzuki+github@gmail.com>
2013-04-24 06:38:12 +0900

    update from juno 3

--- HelloJava/src/org/sweetsmile/practice/Hello.java
+++ HelloJava/src/org/sweetsmile/practice/Hello.java
@@ -8,6 +8,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Hello Java!");
+		System.out.println("Hello Java again!");
 	}
 
 }
